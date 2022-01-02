2815ce1926706654c7403e1b8268ac321fb597fd
super-engineer <akshayshinde7@gmail.com>
2022-01-02 13:51:10 +0530

    1. Added extra attributes to tasks 2. Created some new getter methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_060502) do
+ActiveRecord::Schema.define(version: 2022_01_01_140512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_12_18_060502) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "status"
+    t.string "status", default: "0"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline"
