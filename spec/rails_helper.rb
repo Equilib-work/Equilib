d3bee3ae756be623990105907742a6deda744430
Sandesh Keshwani <sandesh.keshwani@gmail.com>
2022-01-06 17:37:25 +0530

    added controller specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,4 +62,26 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+ require 'devise'
+  
+  RSpec.configure do |config|
+    # For Devise > 4.1.1
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include Devise::Test::IntegrationHelpers, type: :request
+    # Use the following instead if you are on Devise <= 4.1.1
+    # config.include Devise::TestHelpers, :type => :controller
+  end
+
+require_relative 'support/controller_macros' # or require_relative './controller_macros' if write in `spec/support/devise.rb`
+
+  RSpec.configure do |config|
+    # For Devise > 4.1.1
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+    # Use the following instead if you are on Devise <= 4.1.1
+    # config.include Devise::TestHelpers, :type => :controller
+    config.extend ControllerMacros, :type => :controller
+  end
+
 end
